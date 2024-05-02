6887f21ab60fe59363152fefc3c68929ceebb72b
Audrey-sirnik <159150103+Audrey-sirnik@users.noreply.github.com>
2024-05-02 13:30:03 +0000

    python started

--- assignment.py
+++ assignment.py
@@ -1,5 +1,9 @@
 def calculatePi():
-  result = 0
-  return result
+  print("welcome to Pi calculator")
+  inputvalue = input("enter a value for n")
+  for n in range (inputvalue):
+    value= ()
+    result = 0
+    return result
 
 print(result)
