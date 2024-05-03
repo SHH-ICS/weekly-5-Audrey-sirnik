4c10f0070eabf73498d5898789b7cd078d8e871b
Audrey-sirnik <159150103+Audrey-sirnik@users.noreply.github.com>
2024-05-03 13:00:19 +0000

    php added

--- handler.php
+++ handler.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.deep_purple-cyan.min.css" />
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
 
-  <body>
-    
-    <?php
-    $inputvalue = "";
-    if ( isset( $_POST['inputvalue'] ) ){
-      $inputvalue = $_POST['inputvalue'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<body>
+
+  <?php
+  $inputvalue = "";
+  if (isset($_POST['inputvalue'])) {
+    $inputvalue = $_POST['inputvalue'];
+  }
+  foreach (range(0, $inputvalue - 1) as $n) {
+    $value = ((-1) ** $n) / (2 * $n + 1);
+    $result = $result + $value;
+    $n = $n + 1;
+  }
+  $result = $result * 4;
+
+  echo "<h1>My Program</h1>\n";
+  echo "<p>My Variable is = " . $myVariable . "</p>\n";
+  ?>
+
+</body>
 </html>
